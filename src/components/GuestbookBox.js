e82e33d4bde7460fb71d57812fbeeec28b093791
gonyy22 <53338566+gonyy22@users.noreply.github.com>
2021-08-03 01:58:27 +0900

    Refactor css

--- src/components/GuestbookBox.js
+++ src/components/GuestbookBox.js
@@ -5,24 +5,13 @@ import faker from 'faker';
 function GuestbookBox(props) {
     return (
       <>
-        <div className="inner-section">
-          <div className="guestBook-container">
+          <div className="guestBook-inner-container">
             <div className="guestBook-box">
-              <div className="userInfoWrap">
                 <img src={faker.image.animals()} className="userImg" />
                 <span className="userName">박동백</span>
-              </div>
               <p>{props.text}</p>
               </div>
-              {/* <div className="guestBook-box">
-              <div className="userInfoWrap">
-                <img src={faker.image.animals()} className="userImg" />
-                <span className="userName">박동백</span>
-              </div>
-              <p>안녕! 내 이름은 박동백이야!</p>
-              </div>  */}
           </div>
-        </div>
       </>
     )
 }
